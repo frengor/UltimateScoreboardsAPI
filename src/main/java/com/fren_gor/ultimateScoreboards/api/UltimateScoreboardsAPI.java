e45c72bd77845f91e47a417560462497d9748848
fren_gor <goro@frengor.com>
2020-12-25 22:55:03 +0100

    Improve Annotations

--- src/main/java/com/fren_gor/ultimateScoreboards/api/UltimateScoreboardsAPI.java
+++ src/main/java/com/fren_gor/ultimateScoreboards/api/UltimateScoreboardsAPI.java
@@ -11,9 +11,8 @@ import com.fren_gor.ultimateScoreboards.api.scoreboards.Scoreboard1_13;
 import com.fren_gor.ultimateScoreboards.api.scoreboards.ScoreboardLite;
 import com.fren_gor.ultimateScoreboards.api.scoreboards.ScoreboardLong;
 import com.fren_gor.ultimateScoreboards.api.scoreboards.ScoreboardStatic;
-import lombok.Getter;
-import lombok.Setter;
 import org.bukkit.entity.Player;
+import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 import java.util.Collection;
@@ -27,93 +26,138 @@ import java.util.function.Supplier;
  */
 public abstract class UltimateScoreboardsAPI {
 
-    @Getter
-    @Setter
-    public static UltimateScoreboardsAPI API;
+    private static UltimateScoreboardsAPI api;
+
+    /**
+     * Get an instance of {@link UltimateScoreboardsAPI}.
+     *
+     * @return An instance of {@link UltimateScoreboardsAPI}. {@code null} if UltimateScoreboard plugin is not enabled.
+     */
+    @Nullable
+    public static UltimateScoreboardsAPI getAPI() {
+        return api;
+    }
+
+    /**
+     * Set an instance of {@link UltimateScoreboardsAPI} to be retrieved with {@link UltimateScoreboardsAPI#getAPI()}.
+     *
+     * @param api An instance of {@link UltimateScoreboardsAPI}. {@code null} if UltimateScoreboards plugin id disabling.
+     */
+    public static void setAPI(@Nullable UltimateScoreboardsAPI api) {
+        UltimateScoreboardsAPI.api = api;
+    }
 
     // =============== Long Scoreboard ===================
 
-    public abstract ScoreboardLong craftLongScoreboard(Line title, List<Line> lines);
+    @NotNull
+    public abstract ScoreboardLong craftLongScoreboard(@NotNull Line title, @NotNull List<Line> lines);
 
-    public abstract ScoreboardLong craftLongScoreboard(Line title, List<Line> lines, long updateFrequency);
+    @NotNull
+    public abstract ScoreboardLong craftLongScoreboard(@NotNull Line title, @NotNull List<Line> lines, long updateFrequency);
 
-    public abstract ScoreboardLong craftLongScoreboard(Line title, List<Line> lines, boolean ensureUniqueness);
+    @NotNull
+    public abstract ScoreboardLong craftLongScoreboard(@NotNull Line title, @NotNull List<Line> lines, boolean ensureUniqueness);
 
-    public abstract ScoreboardLong craftLongScoreboard(Line title, List<Line> lines, long updateFrequency, boolean ensureUniqueness);
+    @NotNull
+    public abstract ScoreboardLong craftLongScoreboard(@NotNull Line title, @NotNull List<Line> lines, long updateFrequency, boolean ensureUniqueness);
 
-    public abstract <P extends Player> ScoreboardLong craftLongScoreboard(Line title, List<Line> lines, @Nullable Collection<P> viewers);
+    @NotNull
+    public abstract <P extends Player> ScoreboardLong craftLongScoreboard(@NotNull Line title, @NotNull List<Line> lines, @Nullable Collection<P> viewers);
 
-    public abstract <P extends Player> ScoreboardLong craftLongScoreboard(Line title, List<Line> lines, @Nullable Collection<P> viewers,
+    @NotNull
+    public abstract <P extends Player> ScoreboardLong craftLongScoreboard(@NotNull Line title, @NotNull List<Line> lines, @Nullable Collection<P> viewers,
                                                                           long updateFrequency);
 
-    public abstract <P extends Player> ScoreboardLong craftLongScoreboard(Line title, List<Line> lines, @Nullable Collection<P> viewers,
+    @NotNull
+    public abstract <P extends Player> ScoreboardLong craftLongScoreboard(@NotNull Line title, @NotNull List<Line> lines, @Nullable Collection<P> viewers,
                                                                           boolean ensureUniqueness);
 
-    public abstract <P extends Player> ScoreboardLong craftLongScoreboard(Line title, List<Line> lines, @Nullable Collection<P> viewers,
+    @NotNull
+    public abstract <P extends Player> ScoreboardLong craftLongScoreboard(@NotNull Line title, @NotNull List<Line> lines, @Nullable Collection<P> viewers,
                                                                           long updateFrequency, boolean ensureUniqueness);
 
     // =============== Lite Scoreboard ===================
 
-    public abstract ScoreboardLite craftLiteScoreboard(Line title, List<Line> lines);
+    @NotNull
+    public abstract ScoreboardLite craftLiteScoreboard(@NotNull Line title, @NotNull List<Line> lines);
 
-    public abstract ScoreboardLite craftLiteScoreboard(Line title, List<Line> lines, long updateFrequency);
+    @NotNull
+    public abstract ScoreboardLite craftLiteScoreboard(@NotNull Line title, @NotNull List<Line> lines, long updateFrequency);
 
-    public abstract <P extends Player> ScoreboardLite craftLiteScoreboard(Line title, List<Line> lines, @Nullable Collection<P> viewers);
+    @NotNull
+    public abstract <P extends Player> ScoreboardLite craftLiteScoreboard(@NotNull Line title, @NotNull List<Line> lines, @Nullable Collection<P> viewers);
 
-    public abstract <P extends Player> ScoreboardLite craftLiteScoreboard(Line title, List<Line> lines, @Nullable Collection<P> viewers,
+    @NotNull
+    public abstract <P extends Player> ScoreboardLite craftLiteScoreboard(@NotNull Line title, @NotNull List<Line> lines, @Nullable Collection<P> viewers,
                                                                           long updateFrequency);
 
     // =============== Static Scoreboard ===================
 
-    public abstract ScoreboardStatic craftStaticScoreboard(Line title, List<Line> lines);
+    @NotNull
+    public abstract ScoreboardStatic craftStaticScoreboard(@NotNull Line title, @NotNull List<Line> lines);
 
-    public abstract ScoreboardStatic craftStaticScoreboard(Line title, List<Line> lines, boolean ensureUniqueness);
+    @NotNull
+    public abstract ScoreboardStatic craftStaticScoreboard(@NotNull Line title, @NotNull List<Line> lines, boolean ensureUniqueness);
 
-    public abstract <P extends Player> ScoreboardStatic craftStaticScoreboard(Line title, List<Line> lines, @Nullable Collection<P> viewers);
+    @NotNull
+    public abstract <P extends Player> ScoreboardStatic craftStaticScoreboard(@NotNull Line title, @NotNull List<Line> lines, @Nullable Collection<P> viewers);
 
-    public abstract <P extends Player> ScoreboardStatic craftStaticScoreboard(Line title, List<Line> lines, @Nullable Collection<P> viewers,
+    @NotNull
+    public abstract <P extends Player> ScoreboardStatic craftStaticScoreboard(@NotNull Line title, @NotNull List<Line> lines, @Nullable Collection<P> viewers,
                                                                               boolean ensureUniqueness);
 
     // =============== v1_13 Scoreboard ===================
 
-    public abstract Scoreboard1_13 craft1_13Scoreboard(Line title, List<Line> lines);
+    @NotNull
+    public abstract Scoreboard1_13 craft1_13Scoreboard(@NotNull Line title, @NotNull List<Line> lines);
 
-    public abstract Scoreboard1_13 craft1_13Scoreboard(Line title, List<Line> lines, long updateFrequency);
+    @NotNull
+    public abstract Scoreboard1_13 craft1_13Scoreboard(@NotNull Line title, @NotNull List<Line> lines, long updateFrequency);
 
-    public abstract <P extends Player> Scoreboard1_13 craft1_13Scoreboard(Line title, List<Line> lines, @Nullable Collection<P> viewers);
+    @NotNull
+    public abstract <P extends Player> Scoreboard1_13 craft1_13Scoreboard(@NotNull Line title, @NotNull List<Line> lines, @Nullable Collection<P> viewers);
 
-    public abstract <P extends Player> Scoreboard1_13 craft1_13Scoreboard(Line title, List<Line> lines, @Nullable Collection<P> viewers,
+    @NotNull
+    public abstract <P extends Player> Scoreboard1_13 craft1_13Scoreboard(@NotNull Line title, @NotNull List<Line> lines, @Nullable Collection<P> viewers,
                                                                           long updateFrequency);
 
     // =============== Lines ===================
 
-    public abstract TextLine craftTextLine(String line);
+    @NotNull
+    public abstract TextLine craftTextLine(@NotNull String line);
 
-    public abstract SlidingLine craftSlidingLine(String line);
+    @NotNull
+    public abstract SlidingLine craftSlidingLine(@NotNull String line);
 
-    public abstract SlidingLine craftSlidingLine(String line, boolean skipBlanks);
+    @NotNull
+    public abstract SlidingLine craftSlidingLine(@NotNull String line, boolean skipBlanks);
 
-    public abstract SlidingLine craftSlidingLine(String line, boolean skipBlanks, boolean reverse);
+    @NotNull
+    public abstract SlidingLine craftSlidingLine(@NotNull String line, boolean skipBlanks, boolean reverse);
 
-    public abstract StepLine craftStepLine(Collection<String> steps);
+    @NotNull
+    public abstract StepLine craftStepLine(@NotNull Collection<String> steps);
 
-    public abstract UpdateFrequencyLine addUpdateFrequency(Line base, long updateFrequency);
+    @NotNull
+    public abstract UpdateFrequencyLine addUpdateFrequency(@NotNull Line base, long updateFrequency);
 
-    public abstract PlaceholderLine addPlaceholdersReplacing(Line base);
+    @NotNull
+    public abstract PlaceholderLine addPlaceholdersReplacing(@NotNull Line base);
 
     // =========================================
 
-    public abstract ScoreboardGroup craftScoreboardGroup(boolean perPlayerScoreboard, Supplier<Scoreboard> scoreboardCrafter);
+    @NotNull
+    public abstract ScoreboardGroup craftScoreboardGroup(boolean perPlayerScoreboard, @NotNull Supplier<Scoreboard> scoreboardCrafter);
 
-    public abstract void hide(Player player);
+    public abstract void hide(@NotNull Player player);
 
-    public abstract boolean isViewing(Player player);
+    public abstract boolean isViewing(@NotNull Player player);
 
     @Nullable
-    public abstract Scoreboard getCurrentScoreboard(Player player);
+    public abstract Scoreboard getCurrentScoreboard(@NotNull Player player);
 
-    public abstract void show(Player player, Scoreboard scoreboard);
+    public abstract void show(@NotNull Player player, @NotNull Scoreboard scoreboard);
 
-    public abstract void showDefaultScoreboard(Player player);
+    public abstract void showDefaultScoreboard(@NotNull Player player);
 
 }
